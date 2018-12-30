be0dd9385d2a5bf19e4e79c433fcd012356223e3
jpetrucci <jpetrucci49@yahoo.com>
2018-12-30 17:18:18 -0500

    Add basic styles for some flare

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>Hello, World!</p>
-        </header>
+        Hello, World!
       </div>
     );
   }
