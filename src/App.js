557eea829f99d1f92a9dd41db6c0f2aac0864e80
jpetrucci <jpetrucci49@yahoo.com>
2018-12-30 17:41:11 -0500

    Add Form, Title, and Weather components
    Title will be the app header,
    Form will handle user submissions for weather location,
    Weather will handle the data processing and return.
    
    Currently class based, will refactor to be functional later.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
+import Weather from './components/Weather'
 
 class App extends Component {
+    
   render() {
     return (
       <div className="App">
-        Hello, World!
+        <Title />
+        <Form />
+        <Weather />
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Title extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Weather Check</h1>
+				<p>Find current weather conditions and more...</p>
+			</div>
+		)
+	}
+
+}
+
+export default Title
